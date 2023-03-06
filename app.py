611f0fb4035324f3a052ae7d331af237dcaed673
Faizal215 <127084108+Faizal215@users.noreply.github.com>
2023-03-07 00:02:33 +0800

    Update app.py

--- app.py
+++ app.py
@@ -39,16 +39,22 @@ def predict():
     
     
     data = [[inputQuery1, inputQuery2, inputQuery3, inputQuery4, inputQuery5]]
-    new_df = pd.DataFrame(data, columns = ['texture_mean', 'perimeter_mean', 'smoothness_mean', 'compactness_mean', 'symmetry_mean'])
+    new_df = pd.DataFrame(data, columns = ['mean', 'max', 'kurtosis', 'variance', 'onenorm'])
     
     single = model.predict(new_df)
     probablity = model.predict_proba(new_df)[:,1]
     
-    if single==1:
-        o1 = "The patient is diagnosed with Breast Cancer"
+    if single==0:
+        o1 = "The pump diagnosed is healthy"
+        o2 = "Confidence: {}".format(probablity*100)
+    elif single==1:
+        o1 = "The pump diagnosed is severe"
+        o2 = "Confidence: {}".format(probablity*100)
+    elif single==0:
+        o1 = "The pump diagnosed is mild"
         o2 = "Confidence: {}".format(probablity*100)
     else:
-        o1 = "The patient is not diagnosed with Breast Cancer"
+        o1 = "The pump  diagnosed is mild2"
         o2 = "Confidence: {}".format(probablity*100)
         
     return render_template('home.html', output1=o1, output2=o2, query1 = request.form['query1'], query2 = request.form['query2'],query3 = request.form['query3'],query4 = request.form['query4'],query5 = request.form['query5'])
