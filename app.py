2828c8fd0fa44856715131be5e8910e362c3bc14
Faizal215 <127084108+Faizal215@users.noreply.github.com>
2023-03-25 15:22:20 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,3 @@
-# -*- coding: utf-8 -*-
-#"""
-#Created on Mon Aug  3 21:11:12 2020
-#
-#@author: pattn
-#"""
-
-
-# coding: utf-8
-
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier 
@@ -15,7 +5,7 @@ from sklearn import metrics
 from flask import Flask, request, render_template
 import pickle
 
-app = Flask("__name__")
+app = Flask(__name__,template_folder='template')
 
 
 
@@ -35,7 +25,7 @@ def predict():
     inputQuery4 = request.form['query4']
     inputQuery5 = request.form['query5']
 
-    model = pickle.load(open("breastCancerModel.pkl", "rb"))
+    model = pickle.load(open("PumpDiagnosticModel.pkl", "rb"))
     
     
     data = [[inputQuery1, inputQuery2, inputQuery3, inputQuery4, inputQuery5]]
@@ -44,15 +34,20 @@ def predict():
     single = model.predict(new_df)
     probablity = model.predict_proba(new_df)[:,1]
     
-    if single==1:
-        o1 = "The pump diagnosed is health"
+    if single==0:
+        o1 = "The pump is in health condition"
         o2 = "Confidence: {}".format(probablity*100)
+    elif single==1:
+        o1 = "The pump is in severe condition"
+        o2 = "Confidence: {}".format(probablity*100)  
+    elif single==2:
+        o1 = "The pump is in mild condition"
+        o2 = "Confidence: {}".format(probablity*100) 
     else:
-        o1 = "The patient diagnosed with mild"
-        o2 = "Confidence: {}".format(probablity*100)
+        o1 = "The pump is in unstable condition"
+        o2 = "Confidence: {}".format(probablity*100) 
         
     return render_template('home.html', output1=o1, output2=o2, query1 = request.form['query1'], query2 = request.form['query2'],query3 = request.form['query3'],query4 = request.form['query4'],query5 = request.form['query5'])
     
 if __name__ == "__main__":
     app.run()
-
